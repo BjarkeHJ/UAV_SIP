8d6c2aa869e4495ddce0470447222a894fed86ea
BjarkeHJ <bjarke_holm@hotmail.com>
2025-12-02 15:11:43 +0100

    lidar proc

--- src/lidar_perception/include/lidar_perception/lidar_perception_node.hpp
+++ src/lidar_perception/include/lidar_perception/lidar_perception_node.hpp
@@ -3,8 +3,14 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
+#include <tf2/transform_datatypes.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/common.h>
+#include <pcl/filters/filter.h>
 
 class LidarPerceptionNode : public rclcpp::Node {
 public:
@@ -12,16 +18,23 @@ public:
 
 private:
     void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+    void pointcloud_preprocess();
 
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::shared_ptr<tf2_ros::TransformListener>  tf_listener_;
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_pub_;
 
     int cloud_count_; 
     std::string cloud_in_topic_;
     std::string global_frame_;
     std::string drone_frame_;
     std::string lidar_frame_;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr latest_cloud_;
+    geometry_msgs::msg::TransformStamped latest_tf_;
+
 };
 
 #endif
--- src/lidar_perception/src/lidar_perception_node.cpp
+++ src/lidar_perception/src/lidar_perception_node.cpp
@@ -1,6 +1,5 @@
 #include "lidar_perception/lidar_perception_node.hpp"
 
-
 LidarPerceptionNode::LidarPerceptionNode() : Node("LidarPerceptionNode") {
     cloud_in_topic_ = this->declare_parameter<std::string>("pointcloud_in", "/lidar_front/points_raw");
     global_frame_ = this->declare_parameter<std::string>("global_frame", "world");
@@ -17,6 +16,10 @@ LidarPerceptionNode::LidarPerceptionNode() : Node("LidarPerceptionNode") {
         std::bind(&LidarPerceptionNode::pointcloud_callback, this, std::placeholders::_1)
     );
 
+    cloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/test_pcd", 10);
+
+    latest_cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>);
+
     RCLCPP_INFO(this->get_logger(), "LidarPerceptionNode Started...");
 }
 
@@ -26,11 +29,43 @@ void LidarPerceptionNode::pointcloud_callback(const sensor_msgs::msg::PointCloud
         return;
     }
 
-    
+    // Capture transform tree
+    try {
+        latest_tf_ = tf_buffer_->lookupTransform(global_frame_, lidar_frame_, msg->header.stamp);
+    }
+    catch (const tf2::TransformException &ex) {
+        RCLCPP_ERROR(this->get_logger(), "Transform Lookup Failed: %s", ex.what());
+    }
 
+    sensor_msgs::msg::PointCloud2 msg_tf;
+    tf2::doTransform(*msg, msg_tf, latest_tf_);
+    pcl::PointCloud<pcl::PointXYZ> tmp_cloud;
+    pcl::fromROSMsg(msg_tf, tmp_cloud);
+
+    // Remove inf/NaN
+    std::vector<int> indices;
+    pcl::removeNaNFromPointCloud(tmp_cloud, *latest_cloud_, indices);
+    
+    // Republish cloud... (temp)
+    sensor_msgs::msg::PointCloud2 msg_clean;
+    pcl::toROSMsg(*latest_cloud_, msg_clean);
+    msg_clean.header.frame_id = global_frame_;
+    msg_clean.header.stamp = msg->header.stamp;
+    cloud_pub_->publish(msg_clean);
+
+    // TODO: Batch accumulator over N scans using transform information? (Flag to run/wait preprocess on batch)
 }
 
+void LidarPerceptionNode::pointcloud_preprocess() {
+    if (!latest_cloud_ || latest_cloud_->points.empty()) return;
 
+    // GND filtering 
+    
+
+    // Denoise
+
+    // Surface normal estimation 
+}
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
